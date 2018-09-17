074862e8ce7c911e80ee15cd65b977fcc8621340
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2018-09-17 23:53:14 +0200

    v0.7/1.0 compatibility

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using ElectricFields
-using Base.Test
+if VERSION.major > 0 || VERSION.minor > 6
+    using Test
+else
+    using Base.Test
+end
 
 const testfile = joinpath(dirname(@__FILE__), "literate_org_tangled_tests.jl")
 if isfile(testfile)
