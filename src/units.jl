c2ad073c7519fc448e7f7cd1cd3dd3bb70113915
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-09-08 12:13:52 +0200

    No units are now interpreted as atomic units

--- src/units.jl
+++ src/units.jl
@@ -4,7 +4,7 @@
 
 # Unit arithmetic is performed "outside" of @u_str, to ensure type
 # stability and allow precompilation
-base_units = Dict{Symbol,Unitful.FreeUnits}(
+const base_units = Dict{Symbol,Unitful.FreeUnits}(
     :λ => u"nm",
     :I₀ => u"W"/u"cm"^2,
     :E₀ => u"V"/u"m",
@@ -26,32 +26,7 @@ base_units = Dict{Symbol,Unitful.FreeUnits}(
 
 function get_unitful_quantity(field_params::Dict{Symbol,Any}, sym::Symbol)
     v = field_params[sym]
-    typeof(v) <: Quantity || sym ∉ keys(base_units) ? v : v*base_units[sym]
+    typeof(v) <: Quantity || sym ∉ keys(base_units) ? v : v*aunit(base_units[sym])
 end
-
-function set_base_units(unit_specs::Dict{Symbol, Any})
-    global base_units
-    unknown_units = setdiff(keys(unit_specs),
-                            keys(base_units))
-    length(unknown_units) != 0 && error("Unknown base unit, $(join(unknown_units, ", "))")
-
-    for (k,v) in unit_specs
-        base_units[k] = v
-    end
-
-    nothing
-end
-
-macro set_base_units(spec)
-    spec.head == :-> ||
-        error("Expected a block with parameters for definition of the field")
-    block = spec.args[2]
-    block.head == :block ||
-        error("Expected a block with parameters for definition of the field")
-
-    set_base_units(parse_block(block, Any))
-end
-
-export @set_base_units
 
 usplit(u) = ustrip(u),unit(u)
