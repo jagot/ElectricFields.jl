d2aea660cfc281a7a609fc2e29b9ab41f3def1fc
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2021-04-14 12:03:58 +0200

    Remove unused code

--- src/relation_dsl.jl
+++ src/relation_dsl.jl
@@ -1,32 +1,3 @@
-# * Parse assignment DSL blocks
-
-function parse_block(block, T)
-    line_no = 0
-    filename = ""
-    error_message = v -> error("$(filename):$(line_no)\n>   $(v)")
-
-    field_params = Dict{Symbol,T}()
-    param_line_nos = Dict()
-
-    for line in block.args
-        if typeof(line) == LineNumberNode
-            line_no,filename = line.line,line.file
-            continue
-        end
-        typeof(line) == Expr || error_message("Expected expression, got $(line)")
-        line.head == Symbol("=") ||
-            error_message("Expected “parameter = value expression”, got $(line)")
-        k = line.args[1]
-        k in keys(field_params) &&
-            error_message("Field parameter $(k) already specified at $(filename):$(param_line_nos[k])")
-        v = line.args[2]
-        field_params[k] = eval(v)
-        param_line_nos[k] = line_no
-    end
-
-    field_params
-end
-
 # * DSL for calculation of quantities
 
 # The somewhat complicated setup with walking the expression tree in
