637db70325caae9cbf18e8d2268306359a13aceb
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2024-03-09 13:49:08 +0100

    Test apodizing windows and apodized fields

---
+++ test/apodized_fields.jl
@@ -0,0 +1,86 @@
+@testset "Apodized fields" begin
+    @field(F) do
+        ω = 1.0
+        I₀ = 1.0
+        ramp = 0.0
+        flat = 3.0
+        env = :tophat
+    end
+
+    t = timeaxis(F)
+    tplot = ustrip.(auconvert.(u"fs", t))
+
+    tmin = auconvert(u"fs", 1.0)
+    tmax = auconvert(u"fs", 14.0)
+
+    x = ElectricFields.winx.(austrip(tmin), austrip(tmax), austrip.(t))
+
+    pre_sel = findall(<(1.0), t)
+    mid_sel = findall(∈(1.0 .. 14.0), t)
+    mid_sel1 = findall(∈(1.0 .. 7.5), t)
+    mid_sel2 = findall(∈(7.5 .. 14.0), t)
+    post_sel = findall(>(14.0), t)
+
+    @testset "Window: $(window)" for window in (ElectricFields.Hann(), ElectricFields.Hamming(),
+                                                ElectricFields.Blackman(),
+                                                ElectricFields.BlackmanExact(),
+                                                ElectricFields.Nuttall(),
+                                                ElectricFields.BlackmanNuttall(),
+                                                ElectricFields.BlackmanHarris(),
+                                                ElectricFields.Kaiser(3), ElectricFields.Kaiser(2),
+                                                ElectricFields.Rect())
+        w = ElectricFields.window_value.(window, x)
+        ∂w = ElectricFields.window_derivative.(window, x)
+
+        @test all(iszero, w[pre_sel])
+        @test all(iszero, ∂w[pre_sel])
+
+        if window isa ElectricFields.Rect
+            @test all(isone, w[mid_sel])
+            @test all(iszero, ∂w[mid_sel])
+        else
+            @test all(<(1), w[mid_sel])
+            # This is of course not a proper test of the accuracy of
+            # the derivative, just that the behaviour is reasonable.
+            @test all(≥(0), ∂w[mid_sel1])
+            @test all(≤(0), ∂w[mid_sel2])
+        end
+
+        @test all(iszero, w[post_sel])
+        @test all(iszero, ∂w[post_sel])
+    end
+
+    Fw = ApodizedField(F, tmin, tmax, ElectricFields.Kaiser(3))
+
+    tw = timeaxis(Fw)
+
+    @test parent(Fw) == F
+    @test span(Fw) ≈ 1.0 .. 14.0
+    @test tw ⊆ t
+
+    Fv = field_amplitude(F, t)
+    Av = vector_potential(F, t)
+
+    Fwv = field_amplitude(Fw, t)
+    Awv = vector_potential(Fw, t)
+
+    Iv = intensity(F, t)
+    Iwv = intensity(Fw, t)
+
+    w = ElectricFields.window_value.(Fw.window, 1.0, 14.0, t)
+
+
+    @test all(iszero, Fwv[pre_sel])
+    # There is no guarantee that the field amplitude is always smaller
+    # for the apodized field, due to the derivative term.
+    @test all(abs.(Iwv[mid_sel]) .≤ abs.(Iv[mid_sel]))
+    # For the same reason, the intensity is not exactly the original
+    # intensity (flat for this particular field) times the window
+    # value squared, hence the high tolerance.
+    @test abs2.(w[mid_sel]) ≈ Iwv[mid_sel] rtol=5e-2
+    @test all(iszero, Fwv[post_sel])
+
+    @test all(iszero, Awv[pre_sel])
+    @test all(abs.(Awv[mid_sel]) .≤ abs.(Av[mid_sel]))
+    @test all(iszero, Awv[post_sel])
+end
